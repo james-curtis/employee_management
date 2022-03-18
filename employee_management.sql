9494066dbfeeaffe6ac2fb42c757f533d45305fc
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-03-18 21:28:07 +0800

    更新注释

--- employee_management.sql
+++ employee_management.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50728
  File Encoding         : 65001
 
- Date: 18/03/2022 21:16:17
+ Date: 18/03/2022 21:28:04
 */
 
 SET NAMES utf8mb4;
@@ -23,12 +23,12 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `em_attachment`;
 CREATE TABLE `em_attachment`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `path` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '路径',
+  `path` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '路径',
   `comment` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '备注',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
-  `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建者',
-  `update_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新者',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '创建者',
+  `update_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '更新者',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '附件表' ROW_FORMAT = Dynamic;
 
@@ -42,29 +42,29 @@ CREATE TABLE `em_attachment`  (
 DROP TABLE IF EXISTS `em_corporate_information`;
 CREATE TABLE `em_corporate_information`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `corporate_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '实体租户名称（企业名称）（企业全称）',
-  `corporate_contact_person_name` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '企业联系人（企业管理员）',
-  `tenants_number` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '实体租户编号',
-  `operations_status` enum('sign_out','enable','pause') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'enable' COMMENT '运营状态',
-  `review_status` enum('verified','not_approved','pending_reward') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'pending_reward' COMMENT '审核状态 ',
-  `time_efficiency_start` datetime NULL DEFAULT NULL COMMENT '时效,开始',
-  `time_efficiency_end` datetime NULL DEFAULT NULL COMMENT '时效,结束',
+  `corporate_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '实体租户名称（企业名称）（企业全称）',
+  `corporate_contact_person_name` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '企业联系人（企业管理员）',
+  `tenants_number` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '实体租户编号',
+  `operations_status` enum('sign_out','enable','pause') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'enable' COMMENT '运营状态',
+  `review_status` enum('verified','not_approved','pending_reward') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'pending_reward' COMMENT '审核状态 ',
+  `time_efficiency_start` datetime NOT NULL COMMENT '时效,开始',
+  `time_efficiency_end` datetime NOT NULL COMMENT '时效,结束',
   `logo` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '企业logo',
-  `industry` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '所属行业',
+  `industry` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '所属行业',
   `legal_entity` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '企业法人',
-  `contact_phone` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '企业联系人电话',
+  `contact_phone` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '企业联系人电话',
   `summary` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '企业简介',
-  `certificate_for_uniform_social_credit_code` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '统一社会信用代码',
+  `certificate_for_uniform_social_credit_code` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '统一社会信用代码',
   `business_license` int(11) NULL DEFAULT NULL COMMENT '企业工商营业执照（三证合一），图片',
-  `legal_entity_identity_card` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '企业法人身份证号',
+  `legal_entity_identity_card` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '企业法人身份证号',
   `legal_entity_identity_card_img_positive` int(11) NULL DEFAULT NULL COMMENT '企业法人身份证，正面（国徽），图片',
   `legal_entity_identity_card_img_rear` int(11) NULL DEFAULT NULL COMMENT '企业法人身份证，反面（人像），图片',
   `contact_person_identity_card_img_positive` int(11) NULL DEFAULT NULL COMMENT '企业联系人身份证号，正面（国徽），图片',
   `contact_person_identity_card_img_rear` int(11) NULL DEFAULT NULL COMMENT '企业联系人身份证号，反面（人像），图片',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
-  `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建者',
-  `update_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新者',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '创建者',
+  `update_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '更新者',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '企业信息管理' ROW_FORMAT = Dynamic;
 
@@ -81,15 +81,15 @@ INSERT INTO `em_corporate_information` VALUES (3, 'XXX信息科技有限公司',
 DROP TABLE IF EXISTS `em_corporate_user_account`;
 CREATE TABLE `em_corporate_user_account`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
-  `account` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '账号',
-  `corporate_id` int(11) NULL DEFAULT NULL COMMENT '所属企业（关联企业信息管理表）',
-  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '常用手机号',
-  `corporate_user_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '企业用户编号',
-  `status` enum('sign_out','enable','pause') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'enable' COMMENT '状态(enable:启用,paus:暂停,sign_out:注销)',
-  `type` enum('admin','user') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT 'user' COMMENT '类型(admin:企业管理员,user:企业用户)',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '姓名',
+  `account` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '账号',
+  `corporate_id` int(11) NOT NULL COMMENT '所属企业（关联企业信息管理表）',
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '常用手机号',
+  `corporate_user_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '企业用户编号',
+  `status` enum('sign_out','enable','pause') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'enable' COMMENT '状态(enable:启用,paus:暂停,sign_out:注销)',
+  `type` enum('admin','user') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'user' COMMENT '类型(admin:企业管理员,user:企业用户)',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
   `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建者',
   `update_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新者',
   PRIMARY KEY (`id`) USING BTREE
@@ -105,14 +105,14 @@ CREATE TABLE `em_corporate_user_account`  (
 DROP TABLE IF EXISTS `em_department`;
 CREATE TABLE `em_department`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `department_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '部门名称',
-  `superior_department` int(11) NULL DEFAULT NULL COMMENT '上级部门id,0为顶级部门',
-  `owner` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '负责人',
+  `department_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '部门名称',
+  `superior_department` int(11) NOT NULL COMMENT '上级部门id,0为顶级部门',
+  `owner` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '负责人',
   `comment` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '备注',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
-  `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建者',
-  `update_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新者',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '创建者',
+  `update_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '更新者',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '部门表' ROW_FORMAT = Dynamic;
 
@@ -126,11 +126,11 @@ CREATE TABLE `em_department`  (
 DROP TABLE IF EXISTS `em_employee`;
 CREATE TABLE `em_employee`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
-  `account` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
-  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号码',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '姓名',
+  `account` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '手机号码',
   `sex` enum('male','female') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '性别',
-  `job` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '职务',
+  `job` enum('administration','customer_service','design','research_and_development','finance','personnel_matters','it','market','operate','product','purchase','sale','other') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '职务(administration:行政,customer_service:客服,design:设计,research_and_development:研发,finance:财务,personnel_matters:人事,it:IT,market:市场,operate:运营,product:产品,purchase:采购,sale:销售,other:其他)',
   `status` enum('formal','on_trial','quit','pending_resignation') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '状态(formal:正式,on_trial:试用,quit:离职,pending_resignation:待离职)',
   `entry_time` datetime NULL DEFAULT NULL COMMENT '入职时间',
   `departure_time` datetime NULL DEFAULT NULL COMMENT '离职时间',
@@ -142,10 +142,10 @@ CREATE TABLE `em_employee`  (
   `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类型',
   `regular_time` datetime NULL DEFAULT NULL COMMENT '转正时间',
   `comment` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '备注',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
-  `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '创建者',
-  `update_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '更新者',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
+  `create_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '创建者',
+  `update_by` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '更新者',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '员工表' ROW_FORMAT = Dynamic;
 
