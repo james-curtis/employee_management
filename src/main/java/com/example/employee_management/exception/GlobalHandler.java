ee422df00123b13a302ca6add5a5987c254c8b24
suger-no <1030699086@qq.com>
2022-03-19 07:34:01 +0800

    全局异常处理

---
+++ src/main/java/com/example/employee_management/exception/GlobalHandler.java
@@ -0,0 +1,20 @@
+package com.example.employee_management.exception;
+
+import com.example.employee_management.common.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = BindException.class)
+    public Result handler(BindException e){
+        log.error("请求参数出错",e);
+        return Result.fail(e.getMessage());
+    }
+}
